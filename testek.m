bdc1f9f31f02bad07b31f79694eb12519b52c6c5
Kralem <45400194+Kralem@users.noreply.github.com>
2020-05-05 12:44:05 +0200

    Add files via upload
    dopisal skripto, da premakne tekstovne datoteke v direktorij

--- testek.m
+++ testek.m
@@ -1,5 +1,5 @@
 s='S00';
-for i=1:3
+for i=1:5
     if i >= 10
         s='S0';
     elseif i >= 100
@@ -8,5 +8,7 @@ for i=1:3
     
     out=strcat(s,num2str(i));
     disp(out);
-    laplac(out, 4)
+    laplac(out, 4) %kličemo funkcijo laplace za izračun značilk
+    movefile(strcat(out,'featureVectorsL.txt'),'rezultatiL'); %premaknemo .txt datoteki
+    movefile(strcat(out,'referenceClassL.txt'),'rezultatiL'); %na levi ime .txt datoteke, na desni ime direktorija
 end
