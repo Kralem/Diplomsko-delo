75cc88b38f40158a4d791f1b1664df9c28999379
Kralem <45400194+Kralem@users.noreply.github.com>
2020-05-19 11:43:45 +0200

    Add files via upload
    Predelava laplacove formule

--- laplac.m
+++ laplac.m
@@ -23,8 +23,6 @@ function laplac(subj, rec)
     disp(recName);
     [sig, fs, tm] = rdsamp(recName, 1:64); %branje posnetka
     [T0, T1, T2] = getIntervals(recName,'event', fs, size(sig,1)); %pridobitev intervalov
-    T1
-    T2
     sig=sig';
     izpis=strcat("Racunam laplaca za subjekta ",recName);
     disp(izpis);
@@ -32,6 +30,7 @@ function laplac(subj, rec)
         sig_tmp(1,n) = sig(13,n) - 1/4 * (sig(11,n) + sig(36,n) + sig(42,n) + sig(53,n));
         sig_tmp(2,n) = sig(9,n) - 1/4 * (sig(11,n) + sig(32,n) + sig(41,n) + sig(49,n));
     end
+    
     sig=sig_tmp;
     izpis=strcat("Berem intervale za subjekta ",recName);
     disp(izpis);
@@ -44,38 +43,172 @@ function laplac(subj, rec)
   end
   
   if size(t1s,2) > 0 && size(t2s,2) > 0 %varovalka, če subjekt nima intervalov zamišljanja T1 ali T2
-  
-      f = [0 8 8 13 13 fs/2]/(fs/2); %definicija filtra
+      
+      
+      %figure;
+      %plot(cell2mat(t1s(1,:)));
+      %figure;
+      %plot(cell2mat(t2s(:,1)));
+      f = [0  7  8  13  14  fs/2]/(fs/2);  %definicija filtra
       a = [0 0 1 1 0 0 ];
       n = 35; 
       b = firls(n, f, a);
+      
+      win_index_shift = 100; % ce ne vizualizira signale zamisljanj obeh rok v isto sliko
+      
       t1s(1)=[];
       t2s(1)=[];
 
       lvt1=[];
       lvt2=[];
 
-      for i=1:size(t1s,2)
-          tmp = cell2mat(t1s(i));
-          tmp = [tmp(1,:).',tmp(size(tmp,1),:).'].';
-          tmp = filter(b,1,tmp);
-          lvt1(i,1) = log(var(tmp(1,:)));
-          lvt1(i,2) = log(var(tmp(2,:)));      
-      end
+lengths_of_intervals_in_samlpes_left = [];
+lengths_of_intervals_in_seconds_left = [];
+%%%%
 
-      for i=1:size(t2s,2)
-          tmp = cell2mat(t2s(i));
-          tmp = [tmp(1,:).',tmp(size(tmp,1),:).'].';
-          tmp = filter(b,1,tmp);
-          lvt2(i,1) = log(var(tmp(1,:)));
-          lvt2(i,2) = log(var(tmp(2,:)));      
-      end
+  for i=1:size(t1s,2)
+      % tmp = (W*filter(b, 1,cell2mat(t1s(i))));
+
+% Signali se prevedejo v prostor stanj 64 -> 64,
+% zanimata nas le prvi (postane PRVI signal v prostoru stanj) 
+% in 64-ti (postane DRUGI signal v prostoru stanj)
+
+        tmp = (cell2mat(t1s(i)));
+
+% disp(length(tmp(1,:)));
+% disp(length(tmp(64,:)));
+% pause
+
+% Slike signalov za intervale zamisljanja leve roke pred filtriranjem (slike od 1 naprej)
+
+figure(i)
+
+%%%%     length(tmp(1,:)))       dolzina zamisljanja aktivnosti leve roke v vzorcih za ta interval
+%%%%     length(tmp(1,:))*1/fs)  dolzina zamisljanja aktivnosti leve roke v sekundah za ta interval
+
+%%%%    tmp(1,:)                 prvi signal v prostoru stanj (na polozaju 1 v matriki) pred filtriranjem
+plot(tmp(1,:))                 % Prvi signal, Slika je v samplih signala po X osi
+ylim([-1000, 1000]);
+hold on                        % naslednji signa bo v isti sliki
+plot(tmp(size(tmp,1),:))       % Drugi signal, Slika je v samplih signala po X osi
+ylim([-1000, 1000]);
+%%%%    tmp(size(tmp,1),:)       drugi signal v prostoru stanj (na polozaju 64 v matriki) pred filtriranjem
+
+lengths_of_intervals_in_samlpes_left = [lengths_of_intervals_in_samlpes_left, length(tmp(1,:))];
+lengths_of_intervals_in_seconds_left = [lengths_of_intervals_in_seconds_left, length(tmp(1,:))*1/fs];
+
+% Tu se filtrirata prvi in drugi signal v prostoru stanj
+      tmp = [tmp(1,:).', tmp(size(tmp,1),:).' ].';
+      tmp = filter(b, 1, tmp);
+
+% Slike signalov za intervale zamisljanja leve roke PO filtriranju (slike od 100 naprej)
+
+figure(i+win_index_shift)
+%%%%     length(tmp(1,:)))       dolzina zamisljanja aktivnosti leve roke v vzorcih za ta interval
+%%%%     length(tmp(1,:))*1/fs)  dolzina zamisljanja aktivnosti leve roke v sekundah za ta interval
+
+%%%%     tmp(1,:)                prvi signal v prostoru stanj PO filtriranju (na polozaju 1 v matriki)
+plot(tmp(1,:))  % Prvi signal,   Slika je v samplih signala po X osi
+ylim([-100, 100]);
+hold on
+plot(tmp(2,:))  % Drugi signal,  Slika je v samplih signala po X osi
+ylim([-100, 100]);
+%%%%     tmp(2,:)                drugi signal v prostoru stanj PO filtriranju (na polozaju 2 v matriki)
+
+%% Na tem mestu se filtrirani intervali zamisljanj leve roke (po dva signala v prostoru stanj)
+%% lahko zapisejo na disk
+
+% Izracun znacilk za intervale zamisljanja aktivnosti leve roke
+      lvt1(i,1) = log(var(tmp(1,:)));
+      lvt1(i,2) = log(var(tmp(2,:)));     
+  end
+
+% Dolzine intervalov zamisljanja aktivnosti leve roke v vzorcih
+lengths_of_intervals_in_samlpes_left
+% Dolzine intervalov zamisljanja aktivnosti leve roke v sekundah
+lengths_of_intervals_in_seconds_left
+
+%%%%
+
+lengths_of_intervals_in_samlpes_right = [];
+lengths_of_intervals_in_seconds_right = [];
+
+  for i=1:size(t2s,2)
+      % tmp = (W*filter(b, 1,cell2mat(t2s(i))));
+
+% Signali se prevedejo v prostor stanj 64 -> 64,
+% zanimata nas le prvi (postane PRVI signal v prostoru stanj) 
+% in 64-ti (postane DRUGI signal v prostoru stanj)
+
+        tmp = (cell2mat(t2s(i)));
+
+% disp(length(tmp(1,:)));
+% disp(length(tmp(64,:)));
+% pause
+
+% Slike signalov za intervale zamisljanja desne roke pred filtriranjem (slike od 200 naprej)
+
+figure(i+2*win_index_shift)
+
+%%%%    length(tmp(1,:)))        dolzina zamisljanja aktivnosti desne roke v vzorcih za ta interval
+%%%%    length(tmp(1,:))*1/fs)   dolzina zamisljanja aktivnosti desne roke v sekundah za ta interval
+
+%%%%    tmp(1,:)                 prvi signal v prostoru stanj (na polozaju 1 v matriki) pred filtriranjem
+plot(tmp(1,:))                 % Prvi signal, Slika je v samplih signala po X osi
+ylim([-1000, 1000]);
+hold on                        % naslednji signa bo v isti sliki
+plot(tmp(size(tmp,1),:))       % Drugi signal, Slika je v samplih signala po X osi
+ylim([-1000, 1000]);
+%%%%    tmp(size(tmp,1),:)       drugi signal v prostoru stanj (na polozaju 64 v matriki) pred filtriranjem
+
+lengths_of_intervals_in_samlpes_right = [lengths_of_intervals_in_samlpes_right, length(tmp(1,:))];
+lengths_of_intervals_in_seconds_right = [lengths_of_intervals_in_seconds_right, length(tmp(1,:))*1/fs];
+
+% Tu se filtrirata prvi in drugi signal v prostoru stanj
+      tmp = [tmp(1,:).', tmp(size(tmp,1),:).' ].';
+      tmp = filter(b, 1, tmp);
+
+% Slike signalov za intervale zamisljanja desne roke PO filtriranju (slike od 300 dalje)
+
+figure(i+3*win_index_shift)
+
+%%%%    length(tmp(1,:)))        dolzina zamisljanja aktivnosti desne roke v vzorcih za ta interval
+%%%%    length(tmp(1,:))*1/fs)   dolzina zamisljanja aktivnosti desne roke v sekundah za ta interval
+
+%%%%    tmp(1,:)                 prvi signal v prostoru stanj PO filtriranju (na polozaju 1 v matriki)
+plot(tmp(1,:))  % Prvi signal,   Slika je v samplih signala po X osi
+ylim([-100, 100]);
+hold on                        % naslednji signa bo v isti sliki
+plot(tmp(2,:))  % Drugi signal,  Slika je v samplih signala po X osi
+ylim([-100, 100]);
+%%%%    tmp(2,:)                 drugi signal v prostoru stanj PO filtriranju (na polozaju 2 v matriki)
+
+%% Na tem mestu se filtrirani intervali zamisljanj leve roke (po dva signala v prostoru stanj)
+%% lahko zapisejo na disk
+
+% Izracun znacilk za intervale zamisljanja aktivnosti desne roke roke
+      lvt2(i,1) = log(var(tmp(1,:)));
+      lvt2(i,2) = log(var(tmp(2,:)));     
+  end
+
+% Dolzine intervalov zamisljanja aktivnosti desne roke v vzorcih
+lengths_of_intervals_in_samlpes_right
+% Dolzine intervalov zamisljanja aktivnosti desne roke v sekundah
+lengths_of_intervals_in_seconds_right
+
+%%%
+
+
+
+figure(4*win_index_shift)  %% Slika 500 Diagram raztrosa znacilk
+
+      scatter(lvt1(:,1), lvt1(:,2));
+      hold on
+      scatter(lvt2(:,1), lvt2(:,2)); 
+     
       dimenzija=size(sig);
       dimenzija %izpis dimenzije, mora biti približno 2x20000
 
-      %scatter(lvt1(:,1), lvt1(:,2)); %diagram raztrosa
-      %hold on
-      %scatter(lvt2(:,1), lvt2(:,2)); 
 
       featVFile = strcat(subject,'featureVectorsL.txt');
       classFile = strcat(subject,'referenceClassL.txt');
