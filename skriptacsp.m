f1b477685a1de90be70e1cc93e3a4470a7647cd8
Kralem <45400194+Kralem@users.noreply.github.com>
2020-06-01 18:01:46 +0200

    Add files via upload
    posodobljeni rezultati z 10imi foldi

--- skriptacsp.m
+++ skriptacsp.m
@@ -7,10 +7,10 @@ for i=1:109
         s='S';
     end
     out=strcat(s,num2str(i));
-    disp(out);
     if i~=89 && i ~= 88 && i ~= 92 && i~=100 && i~=104 %S089 je problematičen subjekt, zato ga izpustimo
+        disp(out);
         vec=strcat(out,'featureVectors.txt');
         ref=strcat(out,'referenceClass.txt');
-        doClassification(vec,ref, {1,1}, 5, 50, 0);
+        doClassification(vec,ref, {1,1}, 10, 50, 0);
     end    
 end
